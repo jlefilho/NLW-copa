8aa0a3aede096ad2bd4ff8ea300f60e7a001cd7a
Jorge Espindola <jlespindolaf@gmail.com>
2022-11-03 10:38:17 -0300

    Mobile setup

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Center, Text } from 'native-base'
+
+export function SignIn() {
+    return (
+        <Center
+            flex={1}
+            bgColor="gray.900"
+        >
+            <Text
+                color="white"
+                fontSize={24}
+                fontFamily=""
+            >
+                Sign In
+            </Text> 
+        </Center>
+    )
+}
