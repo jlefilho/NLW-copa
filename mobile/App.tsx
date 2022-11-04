e62ee09acb705478150f8a9a798915cd0e296272
Jorge Espindola <jlespindolaf@gmail.com>
2022-11-04 13:19:23 -0300

    JWT Token created and validated

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,7 +6,6 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
-import { Pools } from './src/screens/Pools';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -21,7 +20,7 @@ export default function App() {
         />
         
         {fontsLoaded 
-          ? <Pools />
+          ? <SignIn />
           : <Loading />
         }       
       </AuthContextProvider>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { poolRoutes } from './routes/pool'
 import { userRoutes } from './routes/user'
@@ -14,6 +15,10 @@ async function start() {
 
     await fastify.register(cors, {
         origin: true
+    })
+
+    await fastify.register(jwt, {
+        secret: 'nlwcopa'
     })
 
     await fastify.register(poolRoutes)
