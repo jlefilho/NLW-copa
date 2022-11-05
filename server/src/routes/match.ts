ef350b94cc0a698365d5e2511a07ea3965916ef0
Jorge Espindola <jlespindolaf@gmail.com>
2022-11-05 09:43:05 -0300

    Matches listing

--- server/src/routes/match.ts
+++ server/src/routes/match.ts
@@ -1,6 +1,44 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "../../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function matchRoutes(fastify: FastifyInstance) {
-   
+
+    //LIST MATCHES
+   fastify.get('/pools/:id/matches', {
+    onRequest: [authenticate]
+}, async (request) => {
+    const getPoolParams = z.object({
+        id: z.string()
+    })
+
+    const { id } = getPoolParams.parse(request.params)
+
+    const matches = await prisma.match.findMany({
+        orderBy: {
+            date: 'desc',
+        },
+        include: {
+            bets: {
+                where: {
+                    player: {
+                        userId: request.user.sub,
+                        poolId: id
+                    }
+                }
+            }
+        }
+    })
+
+    return { 
+        matches: matches.map(match => {
+            return {
+                ...match,
+                bet: match.bets.length > 0 ? match.bets[0] : null,
+                bets: undefined
+            }
+        })
+    }
+})
 }
